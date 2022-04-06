2538504865fe5f6e566a4f1d77fa03dd21bd388a
Sangeetha_98 <sangeetha.b@qbrainx.com>
2022-04-06 14:41:36 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num=8;
+        System.out.println(Factorial.factorial(num));
+
+        int palindromeNum=12321;
+        System.out.println(Palindrome.palindrome(palindromeNum));
+
+        int fib=3;
+        System.out.println(Fibanocci.fibo(fib));
+    }
+}
